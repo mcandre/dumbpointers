d8223f7453d6eb341752bcaa7935c095e94b467c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 20:36:15 -0600

    update editorconfig

--- src/lib.rs
+++ src/lib.rs
@@ -12,38 +12,38 @@ use std::thread;
 
 /// A pointer timebomb
 pub struct Tc<T : ?Sized> {
-  bx : Box<T>,
-  expiration : Instant
+    bx : Box<T>,
+    expiration : Instant
 }
 
 /// Construction of Tc given a subtype
 impl<T> Tc<T> {
-  pub fn new(value : T, ttl : Duration) -> Tc<T> {
-    Tc {
-      bx: Box::new(value),
-      expiration: Instant::now() + ttl
+    pub fn new(value : T, ttl : Duration) -> Tc<T> {
+        Tc {
+            bx: Box::new(value),
+            expiration: Instant::now() + ttl
+        }
     }
-  }
 }
 
 /// Query a Tc for its contents.
 impl<T : ?Sized> Tc<T> {
-  fn examine(&self) -> Option<&T> {
-    if Instant::now() < self.expiration {
-      Option::Some(&self.bx)
-    } else {
-      Option::None
+    fn examine(&self) -> Option<&T> {
+        if Instant::now() < self.expiration {
+            Option::Some(&self.bx)
+        } else {
+            Option::None
+        }
     }
-  }
 }
 
 /// Query a Tc for its contents, or panic.
 impl<T : ?Sized> Deref for Tc<T> {
-  type Target = T;
+    type Target = T;
 
-  fn deref(&self) -> &T {
-    self.examine().unwrap()
-  }
+    fn deref(&self) -> &T {
+        self.examine().unwrap()
+    }
 }
 
 /// Unit tests
--- tinyrick.rs
+++ tinyrick.rs
@@ -5,53 +5,53 @@ extern crate tinyrick_extras;
 
 /// Generate documentation
 fn doc() {
-  tinyrick_extras::build();
+    tinyrick_extras::build();
 }
 
 /// Run clippy
 fn clippy() {
-  tinyrick_extras::clippy();
+    tinyrick_extras::clippy();
 }
 
 /// Validate documentation and run linters
 fn lint() {
-  tinyrick::deps(doc);
-  tinyrick::deps(clippy);
+    tinyrick::deps(doc);
+    tinyrick::deps(clippy);
 }
 
 /// Lint and run tests
 fn test() {
-  tinyrick::deps(lint);
-  tinyrick_extras::unit_test();
+    tinyrick::deps(lint);
+    tinyrick_extras::unit_test();
 }
 
 /// BUILD: Doc, lint, test, compile project
 fn build() {
-  tinyrick::deps(test);
-  tinyrick_extras::build();
+    tinyrick::deps(test);
+    tinyrick_extras::build();
 }
 
 /// Publish to crate repository
 fn publish() {
-  tinyrick_extras::publish();
+    tinyrick_extras::publish();
 }
 
 /// Clean workspaces
 fn clean() {
-  tinyrick_extras::clean_cargo();
+    tinyrick_extras::clean_cargo();
 }
 
 /// CLI entrypoint
 fn main() {
-  tinyrick::phony!(clean);
+    tinyrick::phony!(clean);
 
-  tinyrick::wubba_lubba_dub_dub!(
-    build;
-    doc,
-    clippy,
-    lint,
-    test,
-    publish,
-    clean
-  );
+    tinyrick::wubba_lubba_dub_dub!(
+        build;
+        doc,
+        clippy,
+        lint,
+        test,
+        publish,
+        clean
+    );
 }
